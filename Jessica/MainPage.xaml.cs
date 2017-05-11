8f27dfa97d535ec20a266464e60be50bc95e150e
yzhang37 <10142130151_ecnu@outlook.com>
2017-05-11 08:29:23 +0800

    Test page_01

--- Jessica/MainPage.xaml.cs
+++ Jessica/MainPage.xaml.cs
@@ -12,6 +12,9 @@ using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
+using Windows.UI.Popups;
+using Windows.UI;
+
 
 // https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x804 上介绍了“空白页”项模板
 
@@ -22,9 +25,26 @@ namespace Jessica
     /// </summary>
     public sealed partial class MainPage : Page
     {
+        double titlebar = 0;
+
         public MainPage()
         {
             this.InitializeComponent();
+
+            var coreTitleBar = Windows.ApplicationModel.Core.CoreApplication.GetCurrentView().TitleBar;
+            var appTitleBar = Windows.UI.ViewManagement.ApplicationView.GetForCurrentView().TitleBar;
+
+            coreTitleBar.ExtendViewIntoTitleBar = true;
+
+            titlebar = coreTitleBar.Height;
+
+            appTitleBar.ButtonBackgroundColor = Colors.Transparent;
+            appTitleBar.ButtonForegroundColor = Colors.Black;
+            appTitleBar.ButtonInactiveBackgroundColor = Colors.Transparent;
+            appTitleBar.ButtonHoverBackgroundColor = Colors.Pink;
+            appTitleBar.ButtonPressedBackgroundColor = Colors.DeepPink;
+
+            Window.Current.SetTitleBar(grdTitleBar);
         }
     }
 }
